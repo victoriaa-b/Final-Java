5ecbda47cd6fff8d6b4a1905383d0294cbf5b47e
B-Penney <162143485+B-Penney@users.noreply.github.com>
2024-12-08 19:29:27 -0330

    Fixed Products.sql and Users.sql

--- src/main/java/com/keyin/database/Users.sql
+++ src/main/java/com/keyin/database/Users.sql
@@ -1,8 +1,8 @@
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS  Users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     user_password VARCHAR(50) NOT NULL,
-    user_role VARCHAR(20) CHECK (role IN ('Buyer', 'Admin', 'Seller'))
+    user_role VARCHAR(20) CHECK (User_role IN ('Buyer', 'Admin', 'Seller'))
 );
